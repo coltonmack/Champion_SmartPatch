dfeaaa1835f006905592a103eed2c2596d118fc8
coltonmack <31513534+coltonmack@users.noreply.github.com>
2020-10-07 21:43:31 -0400

    more updates

--- smart_patch/lib/home_screen.dart
+++ smart_patch/lib/home_screen.dart
@@ -4,6 +4,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Home Screen')),
       body: Column(children: <Widget>[
         Text("Home Screen"),
         Image.asset('assets/Champion.png')
